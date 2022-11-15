3f69574cf61414cdcb341841fceb66271709c436
Rahul Roy <rahul.roy@uber.com>
2022-11-14 17:55:16 -0800

    fix root default routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rottenpotatoes::Application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
 
   resources :todo_list, controller: 'todo_list'
-  # map '/' to be a redirect to '/movies'
-  # root :to => redirect('/todo_list')
+  root :to => redirect('/todo_list')
 end
