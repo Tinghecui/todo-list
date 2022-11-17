48e827b5ce6f1a6760887c33fd94ab5e2ad1a80e
chrisliuuuu <46462570+chrisliuuuu@users.noreply.github.com>
2022-11-16 20:56:19 -0500

    added tests

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -8,4 +8,12 @@ module ControllerMacros
             sign_in user
         end
     end
+
+    def signout_user
+        before(:each) do
+            @request.env["devise.mapping"] = Devise.mappings[:user]
+            user = FactoryBot.create(:user)
+            sign_out user
+        end
+    end
 end
