3840da97f2d82f4efab15c50b0e85f9a90b63744
Tinghecui <jamescui@bu.edu>
2022-12-19 21:42:21 -0500

    do more spec

--- spec/controllers/todo_list_controller_spec.rb
+++ spec/controllers/todo_list_controller_spec.rb
@@ -24,6 +24,37 @@ RSpec.describe TodoListController, type: :controller do
     end
   end
 
+  describe "GET " do
+    login_user
+    context 'index with time frame' do
+      it 'should return 200:OK' do
+        get :index, todo_list:{:time_frame=> '1d'}
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+
+  describe "GET " do
+    login_user
+    context 'index with done' do
+      it 'should return 200:OK' do
+        get :index, todo_list:{:done=> 'Complete'}
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "GET " do
+    login_user
+    context 'index with pirority' do
+      it 'should return 200:OK' do
+        get :index, :priorities => ['High']
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
   describe "GET index" do
     it "should render index template" do
       get :index
@@ -36,7 +67,11 @@ RSpec.describe TodoListController, type: :controller do
     it 'should create a new task' do
       expect {post :create, todo_list: {:name=> "hw2", :priority => "High"}}.to change {TodoList.count}.by(1)
     end
+    it 'should fail to create a new task' do
+      expect {post :create, todo_list: {:name => "hw2", :priority =>"Medium", :due_date => "2021-11-1"}}.to change {TodoList.count}.by(0)
+    end
   end
+
 
   describe "EDIT the task" do
     login_user
@@ -50,6 +85,18 @@ RSpec.describe TodoListController, type: :controller do
       task = TodoList.create(name: 'hw3', due_date: '2023-1-26')
       post :update, :id => 1, :todo_list => {:priority=> "Low"}
       expect(response).to redirect_to todo_list_path(1)
+    end
+    it "should update info" do
+      TodoList.delete_all
+      task = TodoList.create(name: 'hw3', due_date: '2023-1-26', task_size: 'In Process')
+      post :update, :id => 1, :todo_list => {:task_size => "Complete"}
+      expect(response).to redirect_to todo_list_path(1)
+    end
+    it "should fail to update info" do
+      TodoList.delete_all
+      task = TodoList.create(name: 'hw3', due_date: '2023-1-26')
+      post :update, :id => 1, :todo_list => {:due_date=> "2022-11-20"}
+      expect(response).to redirect_to edit_todo_list_path(1)
     end
   end
 
@@ -69,6 +116,8 @@ RSpec.describe TodoListController, type: :controller do
       expect {TodoList.find(task.id)}.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
+
+
   # describe 'Use movie helper' do
   #   login_user
   #   it 'should Use movie helper' do
